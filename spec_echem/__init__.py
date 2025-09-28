f87c7085613640f7f886da8a4be5fab02720992b
Dean Waldow <waldowda@plu.edu>
2025-09-28 16:44:05 -0700

    Additional small changes

---
+++ spec_echem/__init__.py
@@ -0,0 +1,9 @@
+"""
+Spectroelectrochemistry Package using Avantes Spectrometers
+"""
+from .spectrometer import AvantesSpectrometer
+
+__version__ = "0.1.0"
+__author__ = "Dean Waldow"
+
+__all__ = ["AvantesSpectrometer"]
---
+++ test.py
@@ -0,0 +1,3 @@
+# This should work if avaspec is installed
+from spec_echem import AvantesSpectrometer
+print("Package imported successfully!")
